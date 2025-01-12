632f04d6a52a7a5443a5ae50538d12e6088ed4cd
Matthew Rogers <alula_irl@proton.me>
2025-01-12 00:42:24 -0600

    add ls, -all supp, and _incog

--- data_access.py
+++ data_access.py
@@ -4,43 +4,62 @@ import os
 from urllib.parse import urljoin
 
 STEP = 100
-da_url = os.environ.get('DATA_ACCESS_URL')
-token = os.environ.get('OA_TOKEN')
+da_url = os.environ.get("DATA_ACCESS_URL")
+token = os.environ.get("OA_TOKEN")
+
 
 def publish(endpoint, body):
-    return requests.post(urljoin(da_url, endpoint), json=body, headers={'OA-Token': token.strip()})
+    return requests.post(
+        urljoin(da_url, endpoint), json=body, headers={"OA-Token": token.strip()}
+    )
+
 
 def publish_data(dataset, data, r):
-    for i in range(0,len(data), STEP): 
-        to_push = data[i:i+STEP] 
+    for i in range(0, len(data), STEP):
+        to_push = data[i : i + STEP]
         resp = publish(f"/datasets/{dataset}", to_push)
         if resp.status_code != 200:
-            r.errors.append({'message': f"publishing: {resp.status_code}", 'message_ext': f"publishing: {resp.json()}"})
+            r.errors.append(
+                {
+                    "message": f"publishing: {resp.status_code}",
+                    "message_ext": f"publishing: {resp.json()}",
+                }
+            )
     return
 
+
 def report(dataset, errors):
-    errors_out = [{
-        'url': da_url + f"/datasets/{dataset}",
-        'occurred_at': e['datetime'],
-        "message": f"scrape_service: {e['message']}",
-        "message_ext": f"scrape_service: {e['message_ext']}",
-    } for e in errors]
-    for i in range(0,len(errors_out), STEP): 
-        to_push = errors_out[i:i+STEP] 
+    errors_out = [
+        {
+            "url": da_url + f"/datasets/{dataset}",
+            "occurred_at": e["datetime"],
+            "message": f"scrape_service: {e['message']}",
+            "message_ext": f"scrape_service: {e['message_ext']}",
+        }
+        for e in errors
+    ]
+    for i in range(0, len(errors_out), STEP):
+        to_push = errors_out[i : i + STEP]
         publish("/errors", to_push)
 
+
 def get_latest_datum(dataset):
-    resp = requests.get(urljoin(da_url, f"datasets/{dataset}?sort=_id,desc"))
-    return resp.json()['data'][0]
+    resp = requests.get(urljoin(da_url, f"datasets/{dataset}?sort=_id,desc&_incognito"))
+    return resp.json()["data"][0]
 
 
-def log_scrape_job(dataset, started_at, ended_at, data_count, data_errors, total_errors):
-    resp = publish("/scrape_jobs", [{
-        'dataset': dataset,
-        'started_at': started_at,
-        'ended_at': ended_at,
-        'data_count': data_errors,
-        'total_errors': total_errors
-    }])
-
-
+def log_scrape_job(
+    dataset, started_at, ended_at, data_count, data_errors, total_errors
+):
+    resp = publish(
+        "/scrape_jobs",
+        [
+            {
+                "dataset": dataset,
+                "started_at": started_at,
+                "ended_at": ended_at,
+                "data_count": data_errors,
+                "total_errors": total_errors,
+            }
+        ],
+    )
--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import importlib
 import subprocess
@@ -8,35 +9,57 @@ from datetime import datetime
 
 import json
 
+
 def main():
     args = sys.argv[1:]
     scripts = []
-    for arg in args:
-        arg = arg.strip()
-        if arg.lower().endswith("-all"):
-            scripts.append({'script': arg[:-4], 'is_all': True})
-        else:
-            scripts.append({'script': arg, 'is_all': False})
+    if "".join(args).lower() == "ls":
+        ls()
+    else:
+        for i, arg in enumerate(args):
+            arg = arg.strip()
+            if i < len(args) - 1 and args[i + 1].lower().endswith("-all"):
+                scripts.append({"script": arg, "is_all": True})
+            elif not arg.lower().endswith("-all"):
+                scripts.append({"script": arg, "is_all": False})
+        print(scripts)
+        for script_obj in scripts:
+            script_name = script_obj["script"]
+            is_all = script_obj["is_all"]
 
-    for script_obj in scripts:
-        script_name = script_obj['script']
-        is_all = script_obj['is_all']
+            latest = None
+            r = Report()
 
-        latest = None
-        r = Report()
+            script_func = importlib.import_module(
+                "scripts." + script_name + "." + script_name
+            )
+            scrape_results = script_func.scrape(latest, r)
+            data_access.publish_data(script_name, scrape_results, r)
 
-        script_func = importlib.import_module("scripts." + script_name + "." + script_name)
-        scrape_results = script_func.scrape(latest, r)
-        data_access.publish_data(script_name, scrape_results, r)
-        
-        with open('report_{}.json'.format(script_name + str(datetime.now())), 'w', encoding='utf-8') as f:
-            json.dump(r.errors, f, ensure_ascii=False, indent=4)
+            with open(
+                "report_{}.json".format(script_name + str(datetime.now())),
+                "w",
+                encoding="utf-8",
+            ) as f:
+                json.dump(r.errors, f, ensure_ascii=False, indent=4)
 
-        with open('data_{}.json'.format(script_name + str(datetime.now())), 'w', encoding='utf-8') as f:
-            json.dump(scrape_results, f, ensure_ascii=False, indent=4)
+            with open(
+                "data_{}.json".format(script_name + str(datetime.now())),
+                "w",
+                encoding="utf-8",
+            ) as f:
+                json.dump(scrape_results, f, ensure_ascii=False, indent=4)
 
-    
-    return 0
+        return 0
+
+
+def ls():
+    print("Displaying available dataset scrapers:")
+    for path, folders, files in os.walk("scripts"):
+        for folder in folders:
+            if "__" not in folder:
+                print(folder)
+
 
 if __name__ == "__main__":
     main()
